812132078e01d6cc0e9a7c9e734a1cd11ebe221c
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-03 07:21:37 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+   
+};
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+};
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+};
+function appendCat(name){
+   return [...cats,name];
+    
+}
+
+let catNow=appendCat("Broom");
+function prependCat(name){
+    return ["Arnold",...cats];
+};
+let preCat=prependCat("Arnold");
+
+function removeLastCat(){
+    return cats.slice(0,cats.length-1);
+};
+let catNew=removeLastCat();
+function removeFirstCat(){
+    return cats.slice(1);
+};
+let slicesCat=removeFirstCat();
